a986455a8b89dcfdddc7345d8490d29b20b754af
Ivan C <cordovaivan8@gmail.com>
2022-08-09 20:50:09 -0700

    added for each loop for timeblocks

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 var now = moment();
 var timeBlocks = $(".time-block");
-var currentHour = moment().format("H");
-
-console.log(currentHour);
+// var currentHour = moment().format("H");
 
 $("#currentDay").text(now.format("dddd, MMM Do YYYY"));
 
-var Blockhour = parseInt($(this).attr("id"));
+function hourUpdate() {
+    var currentHour = moment().hour();
+    console.log(currentHour);
 
-console.log(Blockhour);
+    timeBlocks.each(function() {
+        var Blockhour = parseInt($(timeBlocks).attr("id").split("")[0]);
+
+        if (Blockhour < currentHour) {
+            timeBlocks.addClass("past");
+            timeBlocks.removeClass("future");
+            timeBlocks.removeClass("present");
+        };
+
+        if (Blockhour > currentHour) {
+            timeBlocks.addClass("future");
+            timeBlocks.removeClass("present");
+            timeBlocks.removeClass("past");
+        };
+
+        if (Blockhour == currentHour) {
+            timeBlocks.addClass("present");
+            timeBlocks.removeClass("past");
+            timeBlocks.removeClass("future");
+        };
+    });
+};
+
+// console.log(currentHour);
+
+// var Blockhour = parseInt($(timeBlocks).attr("id").split("")[0]);
+
+// console.log(Blockhour);
+
+// timeBlocks.each(function() {
+//     var Blockhour = $(timeBlocks).attr("id").split("")[0];
+
+//     if (Blockhour < currentHour) {
+//         timeBlocks.addClass("past");
+//     };
+
+//     if (Blockhour > currentHour) {
+//         timeBlocks.addClass("future");
+//     };
+
+//     if (Blockhour == currentHour) {
+//         timeBlocks.addClass("present");
+//     };
+// });
+
+// console.log(Blockhour);
 
 
 
